9a1d265fd12c6042f0134ffe0ff9eba15f13a17e
chadoverbey <46037139+chadoverbey@users.noreply.github.com>
2019-02-28 18:39:15 -0600

    Update main.py

--- build-a-blog/main.py
+++ build-a-blog/main.py
@@ -4,7 +4,7 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8888/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
